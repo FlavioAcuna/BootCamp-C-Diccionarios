97e4ee3b2465ad3e56a7b0f1d43f625fb4af59b7
FlavioAcuña <flavio.acuna.t@gmail.com>
2023-10-10 10:19:49 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//
+Dictionary<string, string> perfil = new Dictionary<string, string>();
+perfil.Add("Nombre", "Alex");
+perfil.Add("Apellido", "Miller");
+perfil.Add("Edad", "25");
+perfil.Add("Curso", "C#/.NET");
+//Seleccionar solo un elemento en base al atributo
+Console.WriteLine(perfil["Edad"]);
+//mostrar elementos del diccionarion
+foreach (var perfiles in perfil)
+{
+    Console.WriteLine(perfiles.Key + " - " + perfiles.Value);
+}
+//
+foreach (KeyValuePair<string, string> renglon in perfil)
+{
+    Console.WriteLine(renglon.Key + " - " + renglon.Value);
+}
